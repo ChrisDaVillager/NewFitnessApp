9bcb16a90d5a31aede86c97c09950c4a275a0bca
Chris Puccetti <villager.2855@gmail.com>
2023-01-16 13:32:35 -0800

    Initial Commit

---
+++ NewFitnessApp/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  NewFitnessApp
+//
+//  Created by Chris Puccetti on 1/15/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBAction func loginButton(_ sender: Any) {
+        // log user in
+        
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        // sign user up
+     
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+    
+}
---
+++ NewFitnessApp/SignUpViewContoller.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  NewFitnessApp
+//
+//  Created by Chris Puccetti on 1/16/23.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
